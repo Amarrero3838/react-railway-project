a603c9817f195038bb7c030fae25f03b050a67a4
Alejandro Martino <alemar3838@students.ecpi.edu>
2026-08-27 19:01:34 -0400

    Updated Railway application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alejandro Martino
+          Hello Alejandro Martino, Great job!
         </p>
         <a
           className="App-link"
